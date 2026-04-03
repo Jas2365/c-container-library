ec547f3cd890a7b1a5998e7ca739d8797634afeb
jas23656 <jastha6wdc@gmail.com>
2026-04-03 19:34:38 +0530

    license update

--- inc/lists/my_list.h
+++ inc/lists/my_list.h
@@ -1,5 +1,16 @@
-#pragma once
+/*
+ * Copyright 2026 Jas2365
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied.
+ */
 
+#pragma once
 #include <universal_macros/uni_macros.h>
 
 // for defining
--- inc/strings/my_string_view.h
+++ inc/strings/my_string_view.h
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2026 Jas2365
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
 #pragma once 
 
 #include <my_string.h>
